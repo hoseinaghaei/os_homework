d1462f581846dc27d5f5aa508fd714b5406cb51c
ce424-012-981-05619 <h.aghaei.araei@gmail.com>
2023-02-17 19:27:31 +0000

    fix direct input/output

--- hw1/shell.c
+++ hw1/shell.c
@@ -20,9 +20,9 @@
 #include "shell.h"
 
 int cmd_quit(tok_t arg[]) {
-  printf("Bye\n");
-  exit(0);
-  return 1;
+    printf("Bye\n");
+    exit(0);
+    return 1;
 }
 
 int cmd_help(tok_t arg[]);
@@ -43,7 +43,7 @@ int cmd_cd(tok_t arg[]) {
     int res = chdir(arg[0]);
     if (res != 0)
         return 1;
-    
+
     return 0;
 }
 
@@ -53,9 +53,9 @@ int cmd_cd(tok_t arg[]) {
 /* Command Lookup table */
 typedef int cmd_fun_t (tok_t args[]); /* cmd functions take token array and return int */
 typedef struct fun_desc {
-  cmd_fun_t *fun;
-  char *cmd;
-  char *doc;
+    cmd_fun_t *fun;
+    char *cmd;
+    char *doc;
 } fun_desc_t;
 
 fun_desc_t cmd_table[] = {
@@ -66,48 +66,48 @@ fun_desc_t cmd_table[] = {
 };
 
 int cmd_help(tok_t arg[]) {
-  int i;
-  for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
-    printf("%s - %s\n",cmd_table[i].cmd, cmd_table[i].doc);
-  }
-  return 1;
+    int i;
+    for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
+        printf("%s - %s\n",cmd_table[i].cmd, cmd_table[i].doc);
+    }
+    return 1;
 }
 
 int lookup(char cmd[]) {
-  int i;
-  for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
-    if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0)) return i;
-  }
-  return -1;
+    int i;
+    for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
+        if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0)) return i;
+    }
+    return -1;
 }
 
 void init_shell()
 {
-  /* Check if we are running interactively */
-  shell_terminal = STDIN_FILENO;
+    /* Check if we are running interactively */
+    shell_terminal = STDIN_FILENO;
 
-  /** Note that we cannot take control of the terminal if the shell
-      is not interactive */
-  shell_is_interactive = isatty(shell_terminal);
+    /** Note that we cannot take control of the terminal if the shell
+        is not interactive */
+    shell_is_interactive = isatty(shell_terminal);
 
-  if(shell_is_interactive){
+    if(shell_is_interactive){
 
-    /* force into foreground */
-    while(tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp()))
-      kill( - shell_pgid, SIGTTIN);
+        /* force into foreground */
+        while(tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp()))
+            kill( - shell_pgid, SIGTTIN);
 
-    shell_pgid = getpid();
-    /* Put shell in its own process group */
-    if(setpgid(shell_pgid, shell_pgid) < 0){
-      perror("Couldn't put the shell in its own process group");
-      exit(1);
+        shell_pgid = getpid();
+        /* Put shell in its own process group */
+        if(setpgid(shell_pgid, shell_pgid) < 0){
+            perror("Couldn't put the shell in its own process group");
+            exit(1);
+        }
+
+        /* Take control of the terminal */
+        tcsetpgrp(shell_terminal, shell_pgid);
+        tcgetattr(shell_terminal, &shell_tmodes);
     }
-
-    /* Take control of the terminal */
-    tcsetpgrp(shell_terminal, shell_pgid);
-    tcgetattr(shell_terminal, &shell_tmodes);
-  }
-  /** YOUR CODE HERE */
+    /** YOUR CODE HERE */
 }
 
 /**
@@ -115,7 +115,7 @@ void init_shell()
  */
 void add_process(process* p)
 {
-  /** YOUR CODE HERE */
+    /** YOUR CODE HERE */
 }
 
 /**
@@ -123,28 +123,28 @@ void add_process(process* p)
  */
 process* create_process(char* inputString)
 {
-  /** YOUR CODE HERE */
-  return NULL;
+    /** YOUR CODE HERE */
+    return NULL;
 }
 
 
 
 int shell (int argc, char *argv[]) {
-  char *s = malloc(INPUT_STRING_SIZE+1);			/* user input string */
-  tok_t *t;			/* tokens parsed from input */
-  int lineNum = 0;
-  int fundex = -1;
-  pid_t pid = getpid();		/* get current processes PID */
-  pid_t ppid = getppid();	/* get parents PID */
-  pid_t cpid, tcpid, cpgid;
+    char *s = malloc(INPUT_STRING_SIZE+1);			/* user input string */
+    tok_t *t;			/* tokens parsed from input */
+    int lineNum = 0;
+    int fundex = -1;
+    pid_t pid = getpid();		/* get current processes PID */
+    pid_t ppid = getppid();	/* get parents PID */
+    pid_t cpid, tcpid, cpgid;
 
-  init_shell();
+    init_shell();
 
- // printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
+    // printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
 
-  lineNum=0;
-  // fprintf(stdout, "%d: ", lineNum);
-        while ((s = freadln(stdin))) {
+    lineNum=0;
+    // fprintf(stdout, "%d: ", lineNum);
+    while ((s = freadln(stdin))) {
         t = getToks(s); /* break the line into tokens */
         fundex = lookup(t[0]); /* Is first token a shell literal */
         if (fundex >= 0)
@@ -154,26 +154,36 @@ int shell (int argc, char *argv[]) {
             if (child_pid < 0) {
                 continue;
             } else if (child_pid == 0) {
-            	for (int i = 1; t[i] && i < MAXTOKS; ++i) {
+                char *read_file_address = (char *) malloc(sizeof(char) * FILENAME_MAX);
+                read_file_address = NULL;
+                int read_index;
+                int update_arg_index = MAXTOKS;
+                for (int i = 1; t[i] && i < MAXTOKS; ++i) {
                     if (strcmp(t[i], "<") == 0) {
-                        FILE *infile = fopen(t[i+1], "r");
-                        char *new_arg = (char *) malloc(sizeof(char) * 128);
-                        int j = i;
-                        while (fscanf(infile, "%s", new_arg) != EOF)  {
-                            t[j] = new_arg;
-                            j++;
-                        }
-                        t[j] = NULL;
-                        break;
+                        read_file_address = t[i + 1];
+                        read_index = i+1;
+                        if (i < update_arg_index)
+                            update_arg_index = i;
                     } else if (strcmp(t[i], ">") == 0) {
                         int file_desc = open(t[i+1], O_CREAT | O_RDWR, S_IRWXU);
                         dup2(file_desc, 1) ;
                         t[i] = NULL;
-                        break;
+                        if (i < update_arg_index)
+                            update_arg_index = i;
                     }
                 }
-                
-		execv(t[0], t);
+                if (read_file_address != NULL) {
+                    FILE *infile = fopen(t[read_index], "r");
+                    char *new_arg = (char *) malloc(sizeof(char) * 128);
+                    int j = update_arg_index;
+                    while (fscanf(infile, "%s", new_arg) != EOF)  {
+                        t[j] = new_arg;
+                        j++;
+                    }
+                    t[j] = NULL;
+                }
+
+                execv(t[0], t);
                 const char *executable_path = t[0];
                 char *path = getenv("PATH");
                 tok_t *path_variables = getToks(path);
@@ -196,5 +206,5 @@ int shell (int argc, char *argv[]) {
         // fprintf(stdout, "%d: ", lineNum);
     }
 
-  return 0;
+    return 0;
 }
