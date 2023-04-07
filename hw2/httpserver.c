083b2d485ec2413e9e6ea87fa6a3f80536f8e4c0
hossein.aghaei <hossein.aghaei@digikala.com>
2023-04-07 16:16:45 +0330

    serve file added.

--- hw2/httpserver.c
+++ hw2/httpserver.c
@@ -41,21 +41,38 @@ int server_proxy_port;
  */
 void serve_file(int fd, char *path) {
 
-  http_start_response(fd, 200);
-  http_send_header(fd, "Content-Type", http_get_mime_type(path));
-  http_send_header(fd, "Content-Length", "0"); // Change this too
-  http_end_headers(fd);
-
-  /* TODO: PART 1 Bullet 2 */
-
+    http_start_response(fd, 200);
+    http_send_header(fd, "Content-Type", http_get_mime_type(path));
+    FILE *ptr;
+    char ch;
+    ptr = fopen(path, "r");
+//    if (NULL == ptr) {
+//        printf("file can't be opened \n");
+//    }
+    char *data = malloc(sizeof(char) * 4096);
+    int len = 0;
+    while (!feof(ptr)) {
+        ch = (char)fgetc(ptr);
+        data[len] = ch;
+        len ++;
+    }
+    close(ptr);
+    data[len] = '\0';
+    char int_str[20];
+    sprintf(int_str, "%d", len);
+    http_send_header(fd, "Content-Length", int_str); // Change this too
+    http_end_headers(fd);
+    http_send_string(fd, data);
+    close(fd);
+    /* TODO: PART 1 Bullet 2 */
 }
 
 void serve_directory(int fd, char *path) {
-  http_start_response(fd, 200);
-  http_send_header(fd, "Content-Type", http_get_mime_type(".html"));
-  http_end_headers(fd);
+    http_start_response(fd, 200);
+    http_send_header(fd, "Content-Type", http_get_mime_type(".html"));
+    http_end_headers(fd);
 
-  /* TODO: PART 1 Bullet 3,4 */
+    /* TODO: PART 1 Bullet 3,4 */
 
 }
 
@@ -75,53 +92,66 @@ void serve_directory(int fd, char *path) {
  */
 void handle_files_request(int fd) {
 
-  struct http_request *request = http_request_parse(fd);
+    struct http_request *request = http_request_parse(fd);
 
-  if (request == NULL || request->path[0] != '/') {
-    http_start_response(fd, 400);
+    if (request == NULL || request->path[0] != '/') {
+        http_start_response(fd, 400);
+        http_send_header(fd, "Content-Type", "text/html");
+        http_end_headers(fd);
+        close(fd);
+        return;
+    }
+
+    if (strstr(request->path, "..") != NULL) {
+        http_start_response(fd, 403);
+        http_send_header(fd, "Content-Type", "text/html");
+        http_end_headers(fd);
+        close(fd);
+        return;
+    }
+
+    /* Remove beginning `./` */
+    char *path = malloc(2 + strlen(request->path) + 1);
+    path[0] = '.';
+    path[1] = '/';
+    memcpy(path + 2, request->path, strlen(request->path) + 1);
+
+    /*
+     * TODO: First is to serve files. If the file given by `path` exists,
+     * call serve_file() on it. Else, serve a 404 Not Found error below.
+     */
+    chdir(server_files_directory);
+    struct stat path_stat;
+    stat(path, &path_stat);
+    if (S_ISREG(path_stat.st_mode)) {
+        serve_file(fd, path);
+    } else {
+        http_start_response(fd, 404);
+        http_send_header(fd, "Content-Type", "text/html");
+        http_end_headers(fd);
+        close(fd);
+        return;
+    }
+    /*
+     * TODO: Second is to serve both files and directories. You will need to
+     * determine when to call serve_file() or serve_directory() depending
+     * on `path`.
+     *
+     * Feel FREE to delete/modify anything on this function.
+     */
+
+    http_start_response(fd, 200);
     http_send_header(fd, "Content-Type", "text/html");
     http_end_headers(fd);
+    http_send_string(fd,
+                     "<center>"
+                     "<h1>Welcome to httpserver!</h1>"
+                     "<hr>"
+                     "<p>Nothing's here yet.</p>"
+                     "</center>");
+
     close(fd);
     return;
-  }
-
-  if (strstr(request->path, "..") != NULL) {
-    http_start_response(fd, 403);
-    http_send_header(fd, "Content-Type", "text/html");
-    http_end_headers(fd);
-    close(fd);
-    return;
-  }
-
-  /* Remove beginning `./` */
-  char *path = malloc(2 + strlen(request->path) + 1);
-  path[0] = '.';
-  path[1] = '/';
-  memcpy(path + 2, request->path, strlen(request->path) + 1);
-
-  /* 
-   * TODO: First is to serve files. If the file given by `path` exists,
-   * call serve_file() on it. Else, serve a 404 Not Found error below.
-   *
-   * TODO: Second is to serve both files and directories. You will need to
-   * determine when to call serve_file() or serve_directory() depending
-   * on `path`.
-   *  
-   * Feel FREE to delete/modify anything on this function.
-   */
-
-  http_start_response(fd, 200);
-  http_send_header(fd, "Content-Type", "text/html");
-  http_end_headers(fd);
-  http_send_string(fd,
-      "<center>"
-      "<h1>Welcome to httpserver!</h1>"
-      "<hr>"
-      "<p>Nothing's here yet.</p>"
-      "</center>");
-
-  close(fd);
-  return;
 }
 
 
@@ -138,62 +168,62 @@ void handle_files_request(int fd) {
  */
 void handle_proxy_request(int fd) {
 
-  /*
-  * The code below does a DNS lookup of server_proxy_hostname and 
-  * opens a connection to it. Please do not modify.
-  */
+    /*
+    * The code below does a DNS lookup of server_proxy_hostname and
+    * opens a connection to it. Please do not modify.
+    */
 
-  struct sockaddr_in target_address;
-  memset(&target_address, 0, sizeof(target_address));
-  target_address.sin_family = AF_INET;
-  target_address.sin_port = htons(server_proxy_port);
+    struct sockaddr_in target_address;
+    memset(&target_address, 0, sizeof(target_address));
+    target_address.sin_family = AF_INET;
+    target_address.sin_port = htons(server_proxy_port);
 
-  struct hostent *target_dns_entry = gethostbyname2(server_proxy_hostname, AF_INET);
+    struct hostent *target_dns_entry = gethostbyname2(server_proxy_hostname, AF_INET);
 
-  int target_fd = socket(PF_INET, SOCK_STREAM, 0);
-  if (target_fd == -1) {
-    fprintf(stderr, "Failed to create a new socket: error %d: %s\n", errno, strerror(errno));
-    close(fd);
-    exit(errno);
-  }
+    int target_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (target_fd == -1) {
+        fprintf(stderr, "Failed to create a new socket: error %d: %s\n", errno, strerror(errno));
+        close(fd);
+        exit(errno);
+    }
 
-  if (target_dns_entry == NULL) {
-    fprintf(stderr, "Cannot find host: %s\n", server_proxy_hostname);
-    close(target_fd);
-    close(fd);
-    exit(ENXIO);
-  }
+    if (target_dns_entry == NULL) {
+        fprintf(stderr, "Cannot find host: %s\n", server_proxy_hostname);
+        close(target_fd);
+        close(fd);
+        exit(ENXIO);
+    }
 
-  char *dns_address = target_dns_entry->h_addr_list[0];
+    char *dns_address = target_dns_entry->h_addr_list[0];
 
-  memcpy(&target_address.sin_addr, dns_address, sizeof(target_address.sin_addr));
-  int connection_status = connect(target_fd, (struct sockaddr*) &target_address,
-      sizeof(target_address));
+    memcpy(&target_address.sin_addr, dns_address, sizeof(target_address.sin_addr));
+    int connection_status = connect(target_fd, (struct sockaddr *) &target_address,
+                                    sizeof(target_address));
 
-  if (connection_status < 0) {
-    /* Dummy request parsing, just to be compliant. */
-    http_request_parse(fd);
+    if (connection_status < 0) {
+        /* Dummy request parsing, just to be compliant. */
+        http_request_parse(fd);
 
-    http_start_response(fd, 502);
-    http_send_header(fd, "Content-Type", "text/html");
-    http_end_headers(fd);
-    http_send_string(fd, "<center><h1>502 Bad Gateway</h1><hr></center>");
-    close(target_fd);
-    close(fd);
-    return;
+        http_start_response(fd, 502);
+        http_send_header(fd, "Content-Type", "text/html");
+        http_end_headers(fd);
+        http_send_string(fd, "<center><h1>502 Bad Gateway</h1><hr></center>");
+        close(target_fd);
+        close(fd);
+        return;
 
-  }
+    }
 
-  /* 
-  * TODO: Your solution for task 3 belongs here! 
-  */
+    /*
+    * TODO: Your solution for task 3 belongs here!
+    */
 }
 
 
 void init_thread_pool(int num_threads, void (*request_handler)(int)) {
-  /*
-   * TODO: Part of your solution for Task 2 goes here!
-   */
+    /*
+     * TODO: Part of your solution for Task 2 goes here!
+     */
 }
 
 /*
@@ -203,150 +233,151 @@ void init_thread_pool(int num_threads, void (*request_handler)(int)) {
  */
 void serve_forever(int *socket_number, void (*request_handler)(int)) {
 
-  struct sockaddr_in server_address, client_address;
-  size_t client_address_length = sizeof(client_address);
-  int client_socket_number;
+    struct sockaddr_in server_address, client_address;
+    size_t client_address_length = sizeof(client_address);
+    int client_socket_number;
 
-  *socket_number = socket(PF_INET, SOCK_STREAM, 0);
-  if (*socket_number == -1) {
-    perror("Failed to create a new socket");
-    exit(errno);
-  }
-
-  int socket_option = 1;
-  if (setsockopt(*socket_number, SOL_SOCKET, SO_REUSEADDR, &socket_option,
-        sizeof(socket_option)) == -1) {
-    perror("Failed to set socket options");
-    exit(errno);
-  }
-
-  memset(&server_address, 0, sizeof(server_address));
-  server_address.sin_family = AF_INET;
-  server_address.sin_addr.s_addr = INADDR_ANY;
-  server_address.sin_port = htons(server_port);
-
-  if (bind(*socket_number, (struct sockaddr *) &server_address,
-        sizeof(server_address)) == -1) {
-    perror("Failed to bind on socket");
-    exit(errno);
-  }
-
-  if (listen(*socket_number, 1024) == -1) {
-    perror("Failed to listen on socket");
-    exit(errno);
-  }
-
-  printf("Listening on port %d...\n", server_port);
-
-  init_thread_pool(num_threads, request_handler);
-
-  while (1) {
-    client_socket_number = accept(*socket_number,
-        (struct sockaddr *) &client_address,
-        (socklen_t *) &client_address_length);
-    if (client_socket_number < 0) {
-      perror("Error accepting socket");
-      continue;
+    *socket_number = socket(PF_INET, SOCK_STREAM, 0);
+    if (*socket_number == -1) {
+        perror("Failed to create a new socket");
+        exit(errno);
     }
 
-    printf("Accepted connection from %s on port %d\n",
-        inet_ntoa(client_address.sin_addr),
-        client_address.sin_port);
+    int socket_option = 1;
+    if (setsockopt(*socket_number, SOL_SOCKET, SO_REUSEADDR, &socket_option,
+                   sizeof(socket_option)) == -1) {
+        perror("Failed to set socket options");
+        exit(errno);
+    }
 
-    // TODO: Change me?
-    request_handler(client_socket_number);
-    close(client_socket_number);
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(server_port);
 
-    printf("Accepted connection from %s on port %d\n",
-        inet_ntoa(client_address.sin_addr),
-        client_address.sin_port);
-  }
+    if (bind(*socket_number, (struct sockaddr *) &server_address,
+             sizeof(server_address)) == -1) {
+        perror("Failed to bind on socket");
+        exit(errno);
+    }
 
-  shutdown(*socket_number, SHUT_RDWR);
-  close(*socket_number);
+    if (listen(*socket_number, 1024) == -1) {
+        perror("Failed to listen on socket");
+        exit(errno);
+    }
+
+    printf("Listening on port %d...\n", server_port);
+
+    init_thread_pool(num_threads, request_handler);
+
+    while (1) {
+        client_socket_number = accept(*socket_number,
+                                      (struct sockaddr *) &client_address,
+                                      (socklen_t *) &client_address_length);
+        if (client_socket_number < 0) {
+            perror("Error accepting socket");
+            continue;
+        }
+
+        printf("Accepted connection from %s on port %d\n",
+               inet_ntoa(client_address.sin_addr),
+               client_address.sin_port);
+
+        // TODO: Change me?
+        request_handler(client_socket_number);
+        close(client_socket_number);
+
+        printf("Accepted connection from %s on port %d\n",
+               inet_ntoa(client_address.sin_addr),
+               client_address.sin_port);
+    }
+
+    shutdown(*socket_number, SHUT_RDWR);
+    close(*socket_number);
 }
 
 int server_fd;
+
 void signal_callback_handler(int signum) {
-  printf("Caught signal %d: %s\n", signum, strsignal(signum));
-  printf("Closing socket %d\n", server_fd);
-  if (close(server_fd) < 0) perror("Failed to close server_fd (ignoring)\n");
-  exit(0);
+    printf("Caught signal %d: %s\n", signum, strsignal(signum));
+    printf("Closing socket %d\n", server_fd);
+    if (close(server_fd) < 0) perror("Failed to close server_fd (ignoring)\n");
+    exit(0);
 }
 
 char *USAGE =
-  "Usage: ./httpserver --files www_directory/ --port 8000 [--num-threads 5]\n"
-  "       ./httpserver --proxy inst.eecs.berkeley.edu:80 --port 8000 [--num-threads 5]\n";
+        "Usage: ./httpserver --files www_directory/ --port 8000 [--num-threads 5]\n"
+        "       ./httpserver --proxy inst.eecs.berkeley.edu:80 --port 8000 [--num-threads 5]\n";
 
 void exit_with_usage() {
-  fprintf(stderr, "%s", USAGE);
-  exit(EXIT_SUCCESS);
+    fprintf(stderr, "%s", USAGE);
+    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv) {
-  signal(SIGINT, signal_callback_handler);
-  signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, signal_callback_handler);
+    signal(SIGPIPE, SIG_IGN);
 
-  /* Default settings */
-  server_port = 8000;
-  void (*request_handler)(int) = NULL;
+    /* Default settings */
+    server_port = 8000;
+    void (*request_handler)(int) = NULL;
 
-  int i;
-  for (i = 1; i < argc; i++) {
-    if (strcmp("--files", argv[i]) == 0) {
-      request_handler = handle_files_request;
-      free(server_files_directory);
-      server_files_directory = argv[++i];
-      if (!server_files_directory) {
-        fprintf(stderr, "Expected argument after --files\n");
-        exit_with_usage();
-      }
-    } else if (strcmp("--proxy", argv[i]) == 0) {
-      request_handler = handle_proxy_request;
+    int i;
+    for (i = 1; i < argc; i++) {
+        if (strcmp("--files", argv[i]) == 0) {
+            request_handler = handle_files_request;
+            free(server_files_directory);
+            server_files_directory = argv[++i];
+            if (!server_files_directory) {
+                fprintf(stderr, "Expected argument after --files\n");
+                exit_with_usage();
+            }
+        } else if (strcmp("--proxy", argv[i]) == 0) {
+            request_handler = handle_proxy_request;
 
-      char *proxy_target = argv[++i];
-      if (!proxy_target) {
-        fprintf(stderr, "Expected argument after --proxy\n");
-        exit_with_usage();
-      }
+            char *proxy_target = argv[++i];
+            if (!proxy_target) {
+                fprintf(stderr, "Expected argument after --proxy\n");
+                exit_with_usage();
+            }
 
-      char *colon_pointer = strchr(proxy_target, ':');
-      if (colon_pointer != NULL) {
-        *colon_pointer = '\0';
-        server_proxy_hostname = proxy_target;
-        server_proxy_port = atoi(colon_pointer + 1);
-      } else {
-        server_proxy_hostname = proxy_target;
-        server_proxy_port = 80;
-      }
-    } else if (strcmp("--port", argv[i]) == 0) {
-      char *server_port_string = argv[++i];
-      if (!server_port_string) {
-        fprintf(stderr, "Expected argument after --port\n");
-        exit_with_usage();
-      }
-      server_port = atoi(server_port_string);
-    } else if (strcmp("--num-threads", argv[i]) == 0) {
-      char *num_threads_str = argv[++i];
-      if (!num_threads_str || (num_threads = atoi(num_threads_str)) < 1) {
-        fprintf(stderr, "Expected positive integer after --num-threads\n");
-        exit_with_usage();
-      }
-    } else if (strcmp("--help", argv[i]) == 0) {
-      exit_with_usage();
-    } else {
-      fprintf(stderr, "Unrecognized option: %s\n", argv[i]);
-      exit_with_usage();
+            char *colon_pointer = strchr(proxy_target, ':');
+            if (colon_pointer != NULL) {
+                *colon_pointer = '\0';
+                server_proxy_hostname = proxy_target;
+                server_proxy_port = atoi(colon_pointer + 1);
+            } else {
+                server_proxy_hostname = proxy_target;
+                server_proxy_port = 80;
+            }
+        } else if (strcmp("--port", argv[i]) == 0) {
+            char *server_port_string = argv[++i];
+            if (!server_port_string) {
+                fprintf(stderr, "Expected argument after --port\n");
+                exit_with_usage();
+            }
+            server_port = atoi(server_port_string);
+        } else if (strcmp("--num-threads", argv[i]) == 0) {
+            char *num_threads_str = argv[++i];
+            if (!num_threads_str || (num_threads = atoi(num_threads_str)) < 1) {
+                fprintf(stderr, "Expected positive integer after --num-threads\n");
+                exit_with_usage();
+            }
+        } else if (strcmp("--help", argv[i]) == 0) {
+            exit_with_usage();
+        } else {
+            fprintf(stderr, "Unrecognized option: %s\n", argv[i]);
+            exit_with_usage();
+        }
     }
-  }
 
-  if (server_files_directory == NULL && server_proxy_hostname == NULL) {
-    fprintf(stderr, "Please specify either \"--files [DIRECTORY]\" or \n"
-                    "                      \"--proxy [HOSTNAME:PORT]\"\n");
-    exit_with_usage();
-  }
+    if (server_files_directory == NULL && server_proxy_hostname == NULL) {
+        fprintf(stderr, "Please specify either \"--files [DIRECTORY]\" or \n"
+                        "                      \"--proxy [HOSTNAME:PORT]\"\n");
+        exit_with_usage();
+    }
 
-  serve_forever(&server_fd, request_handler);
+    serve_forever(&server_fd, request_handler);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
