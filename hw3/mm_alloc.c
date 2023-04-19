6ce4ce1e7ac29105328b30e081816d8652acdcea
hossein.aghaei <hossein.aghaei@digikala.com>
2023-04-19 18:27:06 +0330

    fix.

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -102,7 +102,7 @@ void *extend_heap(s_block_ptr last, size_t s) {
     new_block->next = NULL;
     new_block->is_free = 0;
     new_block->size = s;
-    new_block->ptr = new_block + BLOCK_SIZE;
+    new_block->ptr = new_block + 1;
     memset(new_block->ptr, 0, s);
     return new_block->ptr;
 }
