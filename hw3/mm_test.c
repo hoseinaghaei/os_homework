848430d85191e360d49ea0949821d182202ac3b8
hoseinaghaei <h.aghaei.araei@gmail.com>
2023-09-01 15:06:04 +0330

    add malloc test

--- hw3/mm_test.c
+++ hw3/mm_test.c
@@ -5,10 +5,17 @@
 int main(int argc, char **argv)
 {
     int *data;
+    int *data2;
+    int *data3;
 
-    data = (int*) mm_malloc(4);
+    data = (int*) mm_malloc(8);
+    data2 = (int*) mm_malloc(24);
+    data3 = (int*) mm_malloc(24);
     data[0] = 1;
+    data[1] = 32;
     mm_free(data);
+    mm_free(data3);
+    mm_free(data2);
     printf("malloc sanity test successful!\n");
     return 0;
 }
